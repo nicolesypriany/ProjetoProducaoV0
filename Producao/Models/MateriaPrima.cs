45f474f6b7f215ee32a82b2a03cb636a974b9719
Nicole Sypriany <nicolesypriany@gmail.com>
2024-10-08 20:53:38 -0300

    Add project files.

---
+++ Producao/Models/MateriaPrima.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producao.Models
+{
+    internal class MateriaPrima
+    {
+        public MateriaPrima(string Nome)
+        {
+            this.Nome = Nome;
+        }
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Fornecedor { get; set;}
+        public string? Unidade { get; set; }
+        public double Preco {  get; set; }
+    }
+}
